14c79e26200def2715a9f65cdf06de32a95a0fd4
Andre <alamontj89@gmail.com>
2024-01-16 15:38:53 -0500

    splash branch cleanup

--- app.vue
+++ app.vue
@@ -1,14 +1,9 @@
 <template>
   <div>
-    <!-- <header>
-      <main-nav />
-    </header> -->
     <NuxtPage />
   </div>
 </template>
 
 <style lang="scss" src="./main.scss"></style>
 
-<script setup lang="ts">
-import MainNav from "/components/MainNav.vue";
-</script>
+<script setup lang="ts"></script>
